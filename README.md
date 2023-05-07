b25143bec5f0f4d449d0caf4f018bfe7711ebbbe
竹永康 <gqylpy@outlook.com>
2023-05-07 16:39:29 +0800

    1.0alpha1

---
+++ README.md
@@ -0,0 +1,6 @@
+[<img alt="LOGO" src="http://www.gqylpy.com/static/img/favicon.ico" height="21" width="21"/>](http://www.gqylpy.com)
+[![Release](https://img.shields.io/github/release/gqylpy/celeste.svg?style=flat-square")](https://github.com/gqylpy/celeste/releases/latest)
+
+# celeste
+
+> The celeste.
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='celeste',
+    version='1.0.alpha1',
+    author='竹永康',
+    author_email='<gqylpy@outlook.com>',
+    url='http://gqylpy.com',
+    project_urls={'Source': 'https://github.com/gqylpy/celeste'},
+    description='The celeste.',
+    long_description=open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown'
+)
