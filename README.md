efda312bdd4e49febf2b117cb7d88dd2590de652
竹永康 <gqylpy@outlook.com>
2023-06-22 11:06:47 +0800

    1.0alpha2

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 [<img alt="LOGO" src="http://www.gqylpy.com/static/img/favicon.ico" height="21" width="21"/>](http://www.gqylpy.com)
 [![Release](https://img.shields.io/github/release/gqylpy/celeste.svg?style=flat-square")](https://github.com/gqylpy/celeste/releases/latest)
+[![Python Versions](https://img.shields.io/pypi/pyversions/celeste)](https://pypi.org/project/celeste)
+[![License](https://img.shields.io/pypi/l/celeste)](https://github.com/gqylpy/celeste/blob/main/LICENSE)
 
 # celeste
 
-> The celeste.
+> 创建蓝图以定义数据结构，使用蓝图以验证数据。
+
+<kbd>pip3 install celeste</kbd>
+
+```python
+from celeste import Celeste
+
+celeste = Celeste({'name': {type: str}})
+celeste.validate({'name': 'Tom'})
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 import setuptools
+import celeste as i
+
+with open(i.__file__, encoding='utf8') as f:
+    for line in f:
+        if line.startswith('"""  # '):
+            break
 
 setuptools.setup(
-    name='celeste',
-    version='1.0.alpha1',
+    name=i.__name__,
+    version=line[7:-1],
     author='竹永康',
     author_email='<gqylpy@outlook.com>',
+    license='MIT',
     url='http://gqylpy.com',
     project_urls={'Source': 'https://github.com/gqylpy/celeste'},
     description='The celeste.',
     long_description=open('README.md', encoding='utf8').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    py_modules=[i.__name__],
+    python_requires='>=3.8, <4',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Object Brokering',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Software Development :: Quality Assurance',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ]
 )
